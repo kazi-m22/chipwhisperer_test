489847f2f4f9ffbde471c65c865dc680fd1191e6
Kazi Mejbaul Islam <222mzb@gmail.com>
2023-12-07 04:21:23 -0500

    working on modelsim, 5 state

--- aes_core.v
+++ aes_core.v
@@ -1,14 +1,7 @@
-module aes_core (
-	input wire clk,
-	input wire load_i,
-	input wire [255:0] key_i,
-	input wire [127:0] data_i,
-	input wire [1:0] size_i,
-	input wire dec_i,
-	output reg [127:0] data_o,
-	output reg busy_o
-);
+`timescale 1ns/1ps
 
+module tb(output reg [127:0] data_o);
+reg clk;
 reg rst_n = 0;
 reg [1:0] counter = 2'b00;
 reg enable = 0;
@@ -21,8 +14,8 @@ reg [2:0] fsm, fsm_new;
 wire trigger;
 reg done = 0;
 wire [386:0]sc_out;
-//reg busy_o;
-//reg load_i = 0;
+reg busy_o;
+reg load_i = 0;
 wire [386:0] scan_chain;
 
 
@@ -35,7 +28,25 @@ localparam RESET = 3'b000;
 localparam INIT = 3'b001;
 localparam ENCRYPT = 3'b010;
 localparam WAIT = 3'b011;
-localparam FINISH = 3'b100;
+localparam COLLECT = 3'b100;
+localparam FINISH = 3'b101;
+
+
+task reset_dut;
+begin
+    enable = 1'b0;
+    i_text = 127'b0;
+    key = 255'b0;
+    pt_sel = 1'b0;
+    key_sel = 1'b0;
+    ct_out_sel = 1'b0;
+
+    rst_n = 1'b0;
+    #(1 * CLK_PERIOD);
+    rst_n = 1'b1;
+    #(2 * CLK_PERIOD);
+end
+endtask
 
 always @(posedge trigger)
 begin
@@ -57,22 +68,20 @@ begin
 end
 
 
-
-task reset_dut;
+always @(posedge clk)
 begin
-    enable = 1'b0;
-    i_text = 127'b0;
-    key = 255'b0;
-    pt_sel = 1'b0;
-    key_sel = 1'b0;
-    ct_out_sel = 1'b0;
+    #2000
+    #300 load_i = ~load_i;
+    #1 load_i = ~load_i;
 
-//    rst_n = 1'b0;
-//    #(1 * CLK_PERIOD);
-    rst_n = 1'b1;
-//    #(2 * CLK_PERIOD);
 end
-endtask
+
+initial begin
+    clk = 1'b0;
+    forever #5 clk = ~clk;
+end
+
+
 
 always @(posedge clk)
 begin
@@ -84,7 +93,7 @@ begin
            reset_dut();
            data_o  <= 0;
            key <= 0;
-           busy_o <= 0;
+           busy_o <= 1;
            count <= 0;
 
        end
@@ -104,11 +113,14 @@ begin
                 count <= count + 1;
             end
             else
-                fsm <= FINISH;
+                fsm <= COLLECT;
+       end
+        COLLECT: begin
+            data_o <= sc_out[127:0];
+            fsm <= FINISH;
        end
        FINISH: begin
-           busy_o <= 1;
-           data_o <= sc_out[127:0];
+           busy_o <= 0;
            fsm <=  RESET;
        end
        default: fsm <= RESET;
